868566dd701e2d3907941e1664acc90650c80843
ianm93 <45110918+iannm93@users.noreply.github.com>
2021-10-07 21:50:09 -0700

    html skeleton, integrated API with front end of webpage

--- app.js
+++ app.js
@@ -1,37 +1,15 @@
-require("dotenv").config();
-const request = require('request');
-const tmi = require('tmi.js');
-var ComfyJS = require("comfy.js");
 
 
-// comfy js init
-ComfyJS.onCommand = ( user, command, message, flags, extra ) => {
-    if( command === "testing" ) {
-      ComfyJS.Say( "replying to !test" );
-    }
-  }
-ComfyJS.Init(process.env.USERNAME_ID, process.env.CHAT_TOKEN);
-// oauth for comfy.js
+document.addEventListener("DOMContentLoaded", () =>{
+    ComfyJS.onCommand = ( user, command, message, flags, extra ) => {
+        console.log(flags.broadcaster)
+    
+        if(flags.broadcaster && command === "test" ) {
+          console.log("response")
+        }
+      }
+      ComfyJS.Init("ianm93__");
 
-// tmi init
-const client = new tmi.Client({
-	options: { debug: true },
-	identity: {
-		username: 'ianm93__',
-		password: process.env.CHAT_TOKEN
-	},
-	channels: [ 'ianm93__']
-});
+    
+})
 
-client.connect();
-//on message is specific chat
-client.on('message', (channel, tags, message, self) => {
-    if(self) return;
-   if (message === "test"){
-// log channel info
-    console.log(channel)
-   
-    }
-	// Ignore echoed messages.
-// console.log(channel, tags, message, self)
-});
---
+++ server.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const request = require('request');
+const tmi = require('tmi.js');
+var ComfyJS = require("comfy.js");
+
+
+// comfy js init
+ComfyJS.onCommand = ( user, command, message, flags, extra ) => {
+    if( command === "testing" ) {
+      ComfyJS.Say( "replying to !test" );
+      console.log("!test was typed in chat")
+    }
+  }
+ComfyJS.Init(process.env.USERNAME_ID, process.env.CHAT_TOKEN);
+// oauth for comfy.js
+
+// tmi init
+const client = new tmi.Client({
+	options: { debug: true },
+	identity: {
+		username: 'ianm93__',
+		password: process.env.CHAT_TOKEN
+	},
+	channels: [ 'ianm93__']
+});
+
+client.connect();
+//on message is specific chat
+client.on('message', (channel, tags, message, self) => {
+    if(self) return;
+  console.log(message)
+	// Ignore echoed messages.
+// console.log(channel, tags, message, self)
+});
