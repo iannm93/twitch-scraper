9e00bdab6693dbe7af968226e078e1388156234e
ianm93 <45110918+iannm93@users.noreply.github.com>
2021-10-08 00:49:46 -0700

    updated psuedo

--- app.js
+++ app.js
@@ -3,17 +3,20 @@ const userChat = document.getElementById("form");
 const userText = document.getElementById("text")
 userChat.addEventListener("click", findChat)
 
+// initialize comfyjs with the value of the text user entered in form
 function findChat(event){
     event.preventDefault()
     ComfyJS.Init(userText.value);
     
 }
-
+// create empty messsages array to store messages that will be populated on page
 const messages = [];
 console.log(messages)
+// when the dom loads, listen for a chat using comfy
 document.addEventListener("DOMContentLoaded", () =>{
     ComfyJS.onChat = (user, message, flags, self, extra) => {
         console.log(user)
+        // when a user chats, execute update chat and pass it the message
         updateChat(`${user}:  ${message}`)
         
     }
@@ -25,17 +28,21 @@ document.addEventListener("DOMContentLoaded", () =>{
 })
 
 const updateChat = (x) => {
-    
+    // push x (the messsage passed to this function where it is called) into empty messages array
     messages.push(x)
     console.log(messages)
+    // if the messages array is greater than 15, remove the first index (0) of the array and remove the first <li> from the dom
     if (messages.length >15){
         messages.shift();
         const toDelete = messageElement.firstChild
         messageElement.removeChild(toDelete)
 
     }
+    // create a list element 
     const list = document.createElement("li")
+    // make the text of said element equal to the message that is passed into the function
     list.innerText = x;
+    // append that element (which now has the hchat messages populated) to the messages (<ul>) element
     messageElement.appendChild(list)
 
 }
