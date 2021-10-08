b8c1ed81068746dfd77941d4ad4caee84bc84d1d
ianm93 <45110918+iannm93@users.noreply.github.com>
2021-10-07 20:37:58 -0700

    made tmi constructor

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 require("dotenv").config();
 const request = require('request');
+
+
+const client = new tmi.Client({
+	options: { debug: true },
+	identity: {
+		username: 'ianm93__',
+		password: process.env.CHAT_TOKEN
+	},
+	channels: [ 'ianm93__']
+});
+
+const tmi = require('tmi.js');
