22ccb5f8a1d19a7642e0f87015a117a98aa18cf3
ianm93 <45110918+iannm93@users.noreply.github.com>
2021-10-07 21:18:53 -0700

    initialized comfy js to respond to basic commands in chat

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 require("dotenv").config();
 const request = require('request');
-
-
 const tmi = require('tmi.js');
+var ComfyJS = require("comfy.js");
+
+
+// comfy js init
+ComfyJS.onCommand = ( user, command, message, flags, extra ) => {
+    if( command === "testing" ) {
+      ComfyJS.Say( "replying to !test" );
+    }
+  }
+ComfyJS.Init(process.env.USERNAME_ID, process.env.CHAT_TOKEN);
+// oauth for comfy.js
+
+// tmi init
 const client = new tmi.Client({
 	options: { debug: true },
 	identity: {
@@ -13,14 +24,14 @@ const client = new tmi.Client({
 });
 
 client.connect();
-
+//on message is specific chat
 client.on('message', (channel, tags, message, self) => {
     if(self) return;
    if (message === "test"){
-
+// log channel info
     console.log(channel)
    
     }
 	// Ignore echoed messages.
-console.log(channel, tags, message, self)
+// console.log(channel, tags, message, self)
 });
